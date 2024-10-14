56e203d0258dfd9a1ead79c68cef276716228cd8
Srinithi R <129388242+Srinithi2211@users.noreply.github.com>
2024-10-14 20:27:14 +0530

    Crime Analysis and Reporting System

---
+++ CASESTUDY/Csharp_part/CARS.entity/CARS.dao/Services/ServiceProviderInterface.cs
@@ -0,0 +1,39 @@
+﻿using CARS.entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CARS.dao.Services
+{
+    public interface ICrimeAnalysisService
+    {
+
+        bool CreateIncident(Incident incident);
+
+
+        bool UpdateIncidentStatus(string status, int incidentID);
+
+
+        List<Incident> GetIncidentsInDateRange(DateTime startDate, DateTime endDate);
+
+
+        List<Incident> SearchIncidents(string incidentType);
+
+
+        Report GenerateIncidentReport(Incident incident);
+
+
+        bool CreateVictim(Victim victim);
+
+
+        bool CreateSuspect(Suspect suspect);
+
+
+        bool CreateOfficer(Officer officer);
+        bool CreateLawEnforcementAgency(LawEnforcementAgency agency);
+        bool CreateEvidence(Evidence evidence);
+        bool CreateReport(Report report);
+    }
+}
---
+++ CASESTUDY/Csharp_part/CARS.entity/CARS.exception/IncidentNumberNotFoundException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CARS.exception
+{
+    public class IncidentNumberNotFoundException : Exception
+    {
+        public IncidentNumberNotFoundException() : base("Incident number not found.")
+        {
+        }
+
+        public IncidentNumberNotFoundException(string message) : base(message)
+        {
+        }
+
+        public IncidentNumberNotFoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
